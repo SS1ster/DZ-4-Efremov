ec25de4146d72f7c079bdc3ebdd030dbe73f94c0
SS1ster <zyferum@mail.ru>
2021-08-12 19:24:05 +0700

    1

--- DZ 4 Efremov/Program.cs
+++ DZ 4 Efremov/Program.cs
@@ -4,9 +4,46 @@ namespace DZ_4_Efremov
 {
     class Program
     {
-        static void Main(string[] args)
+        static public void SumNumbers()
         {
-            Console.WriteLine("Hello World!");
+        double result = 0;
+        double num = 0;
+        do
+        {
+            num = InputNum();
+        if (num < 0)
+        {
+        Console.WriteLine("Число отрицательное.");
+        }
+                else
+                {
+                    if (num % 2 == 0)
+                    {
+                        Console.WriteLine("Число четное.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число нечетное. Число складывается");
+                        result += num;
+                    }
+                }
+            } while (num != 0);
+            Console.WriteLine("Сумма нечетных чисел = " + result);
+            Console.ReadLine();
+        }
+        static double InputNum()
+        {
+            double number = 0;
+            string str = String.Empty;
+            bool successs = false;
+            do
+            {
+                Console.WriteLine("Введите число, для выхода введите 0.");
+                str = Console.ReadLine();
+                successs = double.TryParse(str, out number);
+                if (!successs) Console.WriteLine("Введите корректное значение! Для завершения введите 0.");
+            } while (!successs);
+            return number;
         }
     }
 }
