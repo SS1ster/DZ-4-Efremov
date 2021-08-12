419b020cb8dbd06335e7b5bb56686724ec3adcb6
SS1ster <zyferum@mail.ru>
2021-08-12 19:16:40 +0700

    Add project files.

---
+++ DZ 4 Efremov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DZ_4_Efremov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
